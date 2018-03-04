339075fd424b5b6e00c6ec32f6c3fc2d7aadb8d3
lipnus <sunpil13@korea.ac.kr>
2018-03-05 04:17:35 +0900

    4

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ var android_user = require('./android/user');
 var android_chat_basicinfo = require('./android/chat_basicinfo');
 var android_chat_behavior = require('./android/chat_behavior');
 var android_chat_aptitude = require('./android/chat_aptitude');
+var android_chat_balance = require('./android/chat_balance');
 
 
 /* GET home page. */
@@ -33,6 +34,7 @@ router.use('/android/user', android_user);
 router.use('/android/chat_basicinfo', android_chat_basicinfo);
 router.use('/android/chat_behavior', android_chat_behavior);
 router.use('/android/chat_aptitude', android_chat_aptitude);
+router.use('/android/chat_balance', android_chat_balance);
 
 
 //엥귤러
